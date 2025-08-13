00d182c2351a7fc5b3cba1e2f519cc30ceb954ab
Victorakinwunmi <adexvictor94@gmail.com>
2025-08-13 18:13:05 +0100

    i allowed connect with me button to show on mobile and adjusted margins

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -3,20 +3,20 @@
       <div class="px-4 md:px-6 lg:px-0 pt-32 gap-2 lg:pt-40 max-w-8xl block items-center lg:flex">
         <div class="w-full lg:w-2/4 text-white type">
            <p class="text-xl lg:text-4xl font-bold">HI THERE , I'M VICTOR ...</p>
-           <div class="mt-5 w-full lg:w-sm lg:mt-8 text-lg lg:text-4xl">
+           <div class="mt-2 w-full lg:w-sm lg:mt-8 text-lg lg:text-4xl">
              BUILDING <span class="text-blue-300 font-bold">DIGITAL WEBSITE </span>EXPERIENCE
            </div>
-           <div class="mt-6">
+           <div class="mt-2 lg:mt-6">
             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quisquam autem rem, ex molestias accusantium, deserunt non odio doloribus   repellendus   ossimus praesentium eum repellat molestiae id veritatis, quod esse earum quaerat?
             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quisquam autem rem, ex molestias accusantium, deserunt non odio doloribus   repellendus   ossimus praesentium eum repellat molestiae id veritatis, quod esse earum quaerat?
            </div>
-           <form @submit.prevent="sendEmail" class="mt-10 hidden md:flex gap-2 w-md">
-             <input type="email" v-model="email" placeholder="Email Address" class="w-4/6 font-semibold bg-white rounded-sm px-6 py-4 text-black outline-0">
-             <button type="submit" class="w-2/6 p-2 bg-blue-300 rounded-sm font-bold cursor-pointer">Connect with me</button>
+           <form @submit.prevent="sendEmail" class="mt-10 flex gap-2 w-full md:w-md">
+             <input type="email" v-model="email" placeholder="Email Address" class=" md:w-4/6 font-semibold bg-white rounded-sm px-6 py-4 text-black outline-0">
+             <button type="submit" class=" md:w-2/6 p-1 sm:text-xs md:text-base truncate md:p-2 bg-blue-300 rounded-sm font-bold cursor-pointer">Connect with me</button>
            </form>
         </div>
         <div class="w-full lg:w-2/4">
-          <div class="rounded-full mt-2 md:mt-6 lg:mt-0 border-t-2 border-white overflow-hidden flex items-center bg-gray-00">
+          <div class="rounded-full mt-4 md:mt-6 lg:mt-0 border-t-2 border-white overflow-hidden flex items-center bg-gray-00">
             <img src="/image/adex.png" class="w-full mt-12"/>
           </div>
         </div>
