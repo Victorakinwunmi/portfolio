6378236c98a8a71e175c354c011103e64253f590
Victorakinwunmi <adexvictor94@gmail.com>
2025-08-13 17:53:25 +0100

    renamed a picture

--- app/components/header/Logo.vue
+++ app/components/header/Logo.vue
@@ -1,6 +1,6 @@
 <template>
  <button class="w-full">
-    <img src="/assets/image/micakin_logo.png" class="mx-auto lg:mx-0 h-10 w-32 lg:w-48 lg:h-12 cursor-pointer">
+    <img src="/image/micakin_logo.png" class="mx-auto lg:mx-0 h-10 w-32 lg:w-48 lg:h-12 cursor-pointer">
  </button> 
 </template>
 
