ecf51a885bad8d713a70e8b64612bc61d7999a5f
Victorakinwunmi <adexvictor94@gmail.com>
2025-08-18 11:45:58 +0100

    i modify the email again

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -91,7 +91,7 @@
 export default {
   data(){
     return{
-     myEmail:'adexvictor94@gmail.com',
+     myEmail:'adexv05@gmail.com',
      user_repecient:"",
     }
   },
@@ -103,10 +103,7 @@ export default {
         const body = encodeURIComponent(
           `Hi, my name is (your name) and I would like you to create me (details).`
         );
-        window.open(
-          `https://mail.google.com/mail/?view=cm&fs=1&to=${this.myEmail}&su=${subject}&body=${body}`,
-          '_blank'
-        );
+       window.location.href = `https://mail.google.com/mail/?view=cm&fs=1&to=${this.myEmail}&su=${subject}&body=${body}`;
       } else {
         alert('Please enter a valid email address');
       }
